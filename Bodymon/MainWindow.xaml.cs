88b0582f18e78add44e7c65f4bebf540794e1db4
Hannes Zelger <hannes.zelger@hak-kitz.ac.at>
2021-10-21 09:02:09 +0200

    First movement trys (experimental)

--- Bodymon/MainWindow.xaml.cs
+++ Bodymon/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Bodymon
 {
@@ -26,26 +27,54 @@ namespace Bodymon
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
+        { 
         }
 
-        
+        bool right;
+        bool left;
+        bool up;
+        bool down;
 
-        
-
-
-    }
-
-    public partial class DispatcherTimerSample : Window
-    {
-        public DispatcherTimerSample()
+        private void MainPage_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            InitializeComponent();
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += timer_Tick;
-            timer.Start();
+            //System.Windows.MessageBox.Show("dsaf");
+
+            up = false;
+            down = false;
+            right = false;
+            left = false;
+            switch (e.Key)
+            {
+                case Key.Right:
+                    right = true;
+                    break;
+                case Key.Left:
+                    left = true;
+                    break;
+                case Key.Up:
+                    up = true;
+                    break;
+                case Key.Down:
+                    down = true;        
+                    break;
+            }
+
+            if (right)
+            {
+                player.Margin = new Thickness(player.Margin.Left+ 5, player.Margin.Top,0,0);
+            }
+            else if (left)
+            {
+                player.Margin = new Thickness(player.Margin.Left- 5, player.Margin.Top, 0, 0);
+            }
+            else if (up)
+            {
+                player.Margin = new Thickness(player.Margin.Left, player.Margin.Top- 5,0,0);
+            }
+            else if (down)
+            {
+                player.Margin = new Thickness(player.Margin.Left, player.Margin.Top+ 5, 0, 0);
+            }
         }
     }
 }
