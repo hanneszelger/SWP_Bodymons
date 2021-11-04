37d304422e7b425a8508f9e958b8f159451f3ccb
Hannes Zelger <hannes.zelger@hak-kitz.ac.at>
2021-11-04 09:08:28 +0100

    movement improved

--- Bodymon/MainWindow.xaml.cs
+++ Bodymon/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,8 @@ namespace Bodymon
         public MainWindow()
         {
             InitializeComponent();
+
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -38,11 +41,6 @@ namespace Bodymon
         private void MainPage_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
             //System.Windows.MessageBox.Show("dsaf");
-
-            up = false;
-            down = false;
-            right = false;
-            left = false;
             switch (e.Key)
             {
                 case Key.Right:
@@ -58,23 +56,80 @@ namespace Bodymon
                     down = true;        
                     break;
             }
+        }
 
-            if (right)
+        private void MainPage_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            switch (e.Key)
             {
-                player.Margin = new Thickness(player.Margin.Left+ 5, player.Margin.Top,0,0);
+                case Key.Right:
+                    right = false;
+                    break;
+                case Key.Left:
+                    left = false;
+                    break;
+                case Key.Up:
+                    up = false;
+                    break;
+                case Key.Down:
+                    down = false;
+                    break;
+            }
+        }
+
+
+        private void Movement()
+        {
+            if (right && player.Margin.Left + player.Width + 5 > area.Margin.Left + area.Width)
+            {
+                schach.Margin = new Thickness(schach.Margin.Left - 5, schach.Margin.Top, 0, 0);
+            }
+            else if (right)
+            {
+                player.Margin = new Thickness(player.Margin.Left + 5, player.Margin.Top, 0, 0);
+            }
+
+             if (left && player.Margin.Left - 5 < area.Margin.Left)
+            {
+                schach.Margin = new Thickness(schach.Margin.Left + 5, schach.Margin.Top, 0, 0);
             }
             else if (left)
             {
-                player.Margin = new Thickness(player.Margin.Left- 5, player.Margin.Top, 0, 0);
+                player.Margin = new Thickness(player.Margin.Left - 5, player.Margin.Top, 0, 0);
+            }
+
+             if (up && player.Margin.Top - 5 < area.Margin.Top)
+            {
+                schach.Margin = new Thickness(schach.Margin.Left, schach.Margin.Top + 5, 0, 0);
             }
             else if (up)
             {
-                player.Margin = new Thickness(player.Margin.Left, player.Margin.Top- 5,0,0);
+                player.Margin = new Thickness(player.Margin.Left, player.Margin.Top - 5, 0, 0);
+            }
+
+             if (down && player.Margin.Top + player.Height + 5 > area.Margin.Top + area.Height)
+            {
+                schach.Margin = new Thickness(schach.Margin.Left, schach.Margin.Top - 5, 0, 0);
             }
             else if (down)
             {
-                player.Margin = new Thickness(player.Margin.Left, player.Margin.Top+ 5, 0, 0);
+                player.Margin = new Thickness(player.Margin.Left, player.Margin.Top + 5, 0, 0);
             }
+
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            Movement();
+           
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 200);
+            dispatcherTimer.Start();
         }
     }
 }
