2a65282ad35d6f601580d51a8da0a2bee5149164
hanneszelger <hannes.zelger@hak-kitz.ac.at>
2021-11-24 11:08:23 +0100

    camera try - not working yet

--- Bodymon/Assets/Classes/Movement.cs
+++ Bodymon/Assets/Classes/Movement.cs
@@ -6,16 +6,19 @@ public class Movement : MonoBehaviour
 {
     float horizontal;
     float vertical;
+
+    private Vector2 camPosition;
+
     private Vector2 move;
     private Rigidbody2D body;
 
-    public float speed = 10.0f;
+    public Camera camera;
+    public float speed = 150.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-
     }
 
     void Update()
@@ -26,13 +29,29 @@ public class Movement : MonoBehaviour
         Debug.Log(horizontal + ";" + vertical);
 
         move = new Vector2(speed * horizontal, speed * vertical);
-        
+        //camPosition = camera.transform.position;
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-        body.velocity = move;
+        //camera.transform.position = Vector3.Lerp(camera.transform.position, new Vector3(camPosition.x + move.x, camPosition.y + move.y, -10), speed);
+
+
+        // Ensure the camera always looks at the player
+        //transform.LookAt(transform.parent);
+
+        if ((body.position.x + move.x*Time.deltaTime > 2 || body.position.x - move.x*Time.deltaTime < -2) && move.x != 0)
+            camera.transform.position = Vector3.Lerp(camera.transform.position, new Vector3(camPosition.x + move.x*Time.deltaTime, camPosition.y, -10), 1);
+        if ((body.position.y + move.y*Time.deltaTime > 2 || body.position.y - move.y*Time.deltaTime < -2) && move.y != 0)
+                camera.transform.position = Vector3.Lerp(camera.transform.position, new Vector3(camPosition.x, camPosition.y + move.y*Time.deltaTime, -10), 1);
+        else
+        {
+                body.velocity = move;
+        }
+
+      
+        
 
 
         //if (Input.GetKeyDown(KeyCode.W))
