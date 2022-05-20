85fec3ec333400232c537831c3dd48f6c28eb24b
domitrixl <dominik.trixl@hak-kitz.ac.at>
2022-05-20 18:39:19 +0200

    AnimationsAdded
    Player now looks way better when walking.
    Might be flexing on you if you are away for too long...

--- Bodymon/Assets/Classes/Player/Movement.cs
+++ Bodymon/Assets/Classes/Player/Movement.cs
@@ -15,6 +15,7 @@ public class Movement : MonoBehaviour
 
     //public allows to edit its value in Unity
     public float speed;
+    public Animator animator;
 
     private void Start()
     {
@@ -36,6 +37,10 @@ public class Movement : MonoBehaviour
 
         //Combines user input and speed to a Vector2
         move = new Vector2(speed * horizontal, speed * vertical);
+
+        animator.SetFloat("Horizontal", horizontal);
+        animator.SetFloat("Vertical", vertical);
+        animator.SetFloat("Speed", move.sqrMagnitude);
     }
 
     private void FixedUpdate()
